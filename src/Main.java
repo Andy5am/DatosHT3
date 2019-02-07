57f6270113c60985a686bfe57576734f2af464c8
Marco Fuentes <marcofuentes1607@gmail.com>
2019-02-06 23:01:40 -0600

    Merge y QuickSort agregados

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,55 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import static java.util.Collections.swap;
 
 public class Main {
 
-    final static String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    public static String randomAlphaNumeric(int count) {
-        StringBuilder builder = new StringBuilder();
-        while (count-- != 0) {
-            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
-            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
-        }
-        return builder.toString();
-    }
-
     public static void main(String args[]){
+        Comparable [] lista = new Comparable[30];
+        //ArrayList<Comparable> lista = new ArrayList();
+        int[] listaNum = new int[30];
 
-
-        ArrayList<Comparable> lista = new ArrayList();
+        Random r = new Random();
 
         for (int i = 0; i<30; i++){
-            lista.add(randomAlphaNumeric(5));
+            lista[i]=(randomAlphaNumeric(5));
+            listaNum[i]= r.nextInt(500)+1;
+        }
+        //Ordenados por quicksort
+        System.out.println("Datos: \n");
+        for (int i = 0;i < lista.length;i++){
+            System.out.print(lista[i]+", ");
+        }
+        quickSort(lista,lista.length);
+        System.out.println();
+        for (int i = 0;i < lista.length;i++){
+            System.out.print(lista[i]+", ");
+        }
+/*
+        //Ordenados por Merge
+        System.out.println("Lista de Números: ");
+        for (int i = 0;i<lista.length;i++){
+            System.out.print(lista[i]+ ", ");
+        }
+        MergeSort ob = new MergeSort();
+        ob.sort(lista,0 ,listaNum.length-1);
+        System.out.println("\nLista Ordenada de Números: \n");
+        for (int i = 0;i<lista.length;i++){
+            System.out.print(lista[i]+ ", ");
         }
 
-        for (int a = 0; a<lista.size();a++){
-            System.out.print(lista.get(a)+", ");
+        //Ordenados por gnomeSort
+        for (int a = 0; a<lista.length;a++){
+            System.out.print(lista[a]+", ");
         }
         System.out.println("\n \n--------------------------- \n \n");
-        ArrayList<Comparable> lista0;
-        lista0 = mergeSortRecursive(lista,new ArrayList<Comparable>(),1,lista.size()-1);
-        for (int i = 0; i<lista0.size() ;i++){
-            System.out.print(lista0.get(i)+", ");
-        }
+        Comparable[] lista0 = new Comparable[lista.length];
+        lista0 = gnomeSort(lista);
+        for (int i = 0; i<lista0.length ;i++){
+            System.out.print(lista0[i]+", ");
+        }*/
     }
 
     public static ArrayList<Integer> selectionSort(ArrayList<Integer> lista){
@@ -53,58 +70,56 @@ public class Main {
         return lista;
     }
 
-    public static ArrayList<Comparable> gnomeSort(ArrayList<Comparable> lista){
-        int size = lista.size();
+    public static Comparable [] gnomeSort(Comparable[] lista){
+        int size = lista.length;
         int i = 0;
         while(i<size){
             if( i==0){
                 i++;
-            }else if(lista.get(i).compareTo(lista.get(i-1))>=0){
+            }else if(lista[i].compareTo(lista[i-1])>=0){
                 i++;
             }else{
                 Comparable temp = 0;
-                temp = lista.get(i);
-                lista.set(i,lista.get(i-1));
-                lista.set(i-1,temp);
+                temp = lista[i];
+                lista[i] =lista[i-1];
+                lista[i-1] =temp;
                 i--;
             }
         }
         return lista;
     }
 
-    private static ArrayList<Comparable> merge (ArrayList<Comparable> data, ArrayList<Comparable> temp, int low, int middle, int high){
-        int ri = low;
-        int ti = low;
-        int di = middle;
+    public static int partition(Comparable data [] ,int left, int right){
+        while(true){
+            while (left < right && data[left].compareTo(data[right])<0) right--;
+            if (left < right) swap(Arrays.asList(data),left++,right);
+            else return left;
 
-        // while two lists are not empty merge smaller value
-        while(ti < middle && di <= high){
-            if(data.get(di).compareTo(temp.get(ti))<0){
-                data.set(ri++, data.get(di++));
-            }else{
-                data.set(ri++, temp.get(ti++));
-            }
+            while (left < right && data[left].compareTo(data[right])<0) left++;
+            if (left < right) swap(Arrays.asList(data),left,right--);
+            else return right;
         }
-        while(ti<middle){
-            data.set(ri++, temp.get(ti++));
-        }
-        return data;
+    }
+    public static void quickSort(Comparable data [], int n){
+        quickSortRecursive(data,0,n-1);
+    }
+    public static void quickSortRecursive(Comparable data[], int left, int right){
+        int pivot;
+        if (left >= right) return;
+        pivot = partition(data,left,right);
+        quickSortRecursive(data, left, pivot-1);
+        quickSortRecursive(data, pivot +1 ,right);
     }
 
-    private static ArrayList<Comparable> mergeSortRecursive (ArrayList<Comparable> data, ArrayList<Comparable> temp, int low, int high){
-        int n = high -low +1;
-        int middle =  low + n/2;
 
-        for (int i = low; i < middle && i<0; i++) {
-            temp.set(i, data.get(i));
+
+    final static String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static String randomAlphaNumeric(int count) {
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
         }
-
-        // sort lower half of array
-        mergeSortRecursive(temp,data,low,middle-1);
-        // sort upper half of array
-        mergeSortRecursive(data,temp,middle,high);
-        // merge halves together
-        merge(data,temp,low,middle,high);
-        return data;
+        return builder.toString();
     }
 }
